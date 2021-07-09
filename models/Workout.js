fe5a73b18be5a52ba619b52d27288ad2632f9463
lillie <lillie@Lillies-MacBook-Pro.local>
2021-07-08 21:49:45 -0700

    create folder models, index.js, workout.js and updated server.js and workout.js

---
+++ models/Workout.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+
+
+
+    
+})
+
+
+
+
+
+
+const LWorkout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;

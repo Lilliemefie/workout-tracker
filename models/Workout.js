a0e6d71dedc643481e18ac622262b7755776ac00
lillie <lillie@Lillies-MacBook-Pro.local>
2021-07-09 03:24:17 -0700

    updated Workout.js

--- models/Workout.js
+++ models/Workout.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    
     exercises: [{
     type: {
         type: String,
@@ -39,10 +44,6 @@ const WorkoutSchema = new Schema({
         trim: true,
         require: "Sets is required"}
 }],
-    day: {
-        type: Date,
-        default: Date.now
-    },
 
     // lastUpdated: Date,
 });

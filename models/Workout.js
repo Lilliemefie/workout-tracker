4e4df2173cb760924feb7407d5fca7b47da5ac5f
lillie <lillie@Lillies-MacBook-Pro.local>
2021-07-08 22:18:05 -0700

    updated Workout.js

--- models/Workout.js
+++ models/Workout.js
@@ -3,17 +3,33 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
+    type: {
+        type: String,
+        trim: true,
+        required: "Type of workout is required"
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: "Name of workout is required"
+    },
+    duration: {
+        type: Number,
+        trim: true,
+        require: "Duration is required"
+    },
+    weight: {
+        type: Number
+    },
+    reps: {
+        type: Number
+    },
+    sets: {
+        type: Number
+    }
+});
 
 
-
-    
-})
-
-
-
-
-
-
-const LWorkout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;

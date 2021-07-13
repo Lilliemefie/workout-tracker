078dd4c60500fd47f37311ba7e9fd37f3950a6c0
lillie <lillie@Lillies-MacBook-Pro.local>
2021-07-12 18:32:00 -0700

    moved routes to server.js as the previous and deleted routes folder

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+const db = require("./models");
+const path = require("path");
+
+
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -20,12 +24,60 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 
 //call first page (link for Fitness Tracker on Nav) > WORK
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
+    res.send(index.html);
   });
 
- // Routes
-app.use(require("./routes/apiRoutes.js"));
-app.use(require("./routes/htmlRoutes.js"));
+ //create new workout > WORK
+ app.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+    .then(newWorkout => {
+      res.json(newWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// Read last workout > WORK
+app.get("/api/workouts", (req, res) => {
+  console.log("Reading workout")
+  Workout.find({})
+    .then(WorkoutDT => {
+      res.json(WorkoutDT);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+// Add an exercise - 
+app.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: {exercises: req.body}},
+    { new: true}
+  )
+    .then(WorkoutDT => {
+      res.json(WorkoutDT);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+})
+
 
 
 
